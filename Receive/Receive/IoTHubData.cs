1a2e7405fe23b76e82b2c1f3149e71282422718e
John Staveley <john@staveley.org>
2022-02-06 10:11:55 +0000

    Divert received message from satellite to blob storage

--- Receive/Receive/IoTHubData.cs
+++ Receive/Receive/IoTHubData.cs
@@ -1,7 +1,9 @@
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 using IoTHubTrigger = Microsoft.Azure.WebJobs.EventHubTriggerAttribute;
 
 namespace Receive
@@ -10,12 +12,18 @@ namespace Receive
     {
 
         [FunctionName("IoTHubData")]
-        public static void Run([IoTHubTrigger("messages/events", Connection = "AzureIoTHubConnectionString", ConsumerGroup = "$Default")] EventData message, ILogger log)
+        public static async Task Run(
+            [IoTHubTrigger("messages/events", Connection = "AzureIoTHubConnectionString", ConsumerGroup = "$Default")] EventData message, 
+            [Blob("outputdemo/{sys.utcnow}.txt", FileAccess.Write, Connection = "AzureWebJobsStorage")] Stream outputFile,
+            ILogger log)
         {
             var payload = Encoding.UTF8.GetString(message.Body.Array);
             var deviceId = message.SystemProperties["iothub-connection-device-id"];
             log.LogInformation($"C# IoT Hub trigger function processed a message: {payload} from {deviceId}");
-
+            UnicodeEncoding uniencoding = new UnicodeEncoding();
+            byte[] output = uniencoding.GetBytes(payload);
+            // TODO: Process Kineis payload into something intelligable
+            await outputFile.WriteAsync(output, 0, output.Length);
         }
     }
 }
